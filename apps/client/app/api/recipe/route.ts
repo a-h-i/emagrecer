5e865fcffe0942e28e535035def635c84c61bb52
Ahmed Ismail <ahm3d.hisham@gmail.com>
2025-09-20 00:58:06 +0100

    implements GET /api/recipe route only

--- apps/client/app/api/recipe/route.ts
+++ apps/client/app/api/recipe/route.ts
@@ -1,11 +1,40 @@
+import { z } from 'zod';
+import { RecipeSort, searchRecipe } from '@emagrecer/control';
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@/auth';
+import { getDS } from '@/lib/getDS';
 
 
+const querySchema = z.object({
+  query: z.string().min(1),
+  tags: z.array(z.string()).optional(),
+  sort: z.enum(RecipeSort),
+  sort_direction: z.enum(['asc', 'desc']),
+  next_page_token: z.string().optional().nullish(),
+});
 
+/**
+ * Recipe search endpoint
+ *
+ */
+export async function GET(req: NextRequest) {
+  const session = await auth();
+  if (session?.user?.id == null) {
+    return NextResponse.json({ error: 'unauthorized' }, { status: 401 });
+  }
+  const filters = querySchema.safeParse({
+    query: req.nextUrl.searchParams.get('query'),
+    tags: req.nextUrl.searchParams.getAll('tags'),
+    sort: req.nextUrl.searchParams.get('sort'),
+    sort_direction: req.nextUrl.searchParams.get('sort_direction'),
+    next_page_token: req.nextUrl.searchParams.get('next_page_token'),
+  });
+  if (!filters.success) {
+    return NextResponse.json({ error: z.treeifyError(filters.error) }, { status: 400 });
+  }
+  const source = await getDS();
+  const searchResult = await searchRecipe(source.manager, filters.data, filters.data.next_page_token);
 
+  return NextResponse.json(searchResult);
 
-
-
-
-export function GET() {
-  throw new Error('not implemented');
 }
--- packages/control/src/recipe/searchRecipe.ts
+++ packages/control/src/recipe/searchRecipe.ts
@@ -1,17 +1,30 @@
 import { EntityManager } from 'typeorm';
-import { Recipe } from '@emagrecer/storage';
+import { Recipe, RecipeSchemaTypeWithTags } from '@emagrecer/storage';
+
+export enum RecipeSort {
+  RELEVANCE = 'relevance',
+  PROTEIN = 'protein',
+  CALORIES = 'calories',
+  TIME = 'time',
+}
 
 export type RecipeFilters = {
   query: string;
-  tags: string[];
-  sort: 'relevance' | 'protein' | 'calories' | 'time'
+  tags?: string[];
+  sort: RecipeSort;
   sort_direction: 'asc' | 'desc';
 }
 
-export async function searchRecipe(manager: EntityManager, filters: RecipeFilters, next_page_token?: string | null) {
+interface RecipeSearchResults {
+  recipes: RecipeSchemaTypeWithTags[];
+  next_page_token?: string;
+}
+
+export async function searchRecipe(manager: EntityManager, filters: RecipeFilters, next_page_token?: string | null): Promise<RecipeSearchResults> {
 
   let query = manager.createQueryBuilder()
     .select('recipe')
     .from(Recipe, 'recipe')
   ;
+
 }
